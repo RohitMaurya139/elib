508e5e490fa29966fb3ed7bcd88a0a2827c448ce
Rohit Maurya <rohit139maurya@gmail.com>
2026-02-20 21:10:27 +0530

    feat: add JWT authentication to user creation logic and update config

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,8 @@ dotenv.config()
 const _config = {
     port: process.env.PORT ? Number(process.env.PORT) : 5000,
     database_Url: process.env.MONGODB_URI,
-    env:process.env.NODE_ENV
+    env: process.env.NODE_ENV,
+    jwtSecret:process.env.JWT_SECRET
 };
 
 export const config=Object.freeze(_config) // on one can overwrite from other file
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,26 +2,40 @@ import type { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import Users from "./userModel.ts";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { config } from "../config/config.ts";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const { name, email, password } = req.body;
     // 1. validation
     if (!name || !email || !password) {
         const error = createHttpError(400, "All fields are required");
-    return next(error)
-}
+        return next(error);
+    }
     // 2. process
 
-    const user = await Users.findOne({ email })
+    const user = await Users.findOne({ email });
     if (user) {
-         const error = createHttpError(400, "User Already exist with this email");
-         return next(error);
+        const error = createHttpError(
+            400,
+            "User Already exist with this email",
+        );
+        return next(error);
     }
 
-    const hashPassword= await bcrypt.hash(password,10)
-    // 3. response
+    const hashPassword = await bcrypt.hash(password, 10);
 
-    res.json({message:"calling register api from userController"})
+    const newUser = await Users.create({
+        name,
+        email,
+        password: hashPassword,
+    });
+
+    // token generation
+    const token = jwt.sign({ sub: newUser._id }, config.jwtSecret as string,{expiresIn:'7d'});
+
+    // 3. response
+    res.json({ accessToken:token });
 }
 
 
